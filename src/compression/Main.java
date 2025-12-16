120c33c5c516eb128ce6907511421cdd7674bbbb
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2025-12-16 17:51:45 +0100

    Version final du projet

--- src/compression/Main.java
+++ src/compression/Main.java
@@ -5,15 +5,12 @@ import utils.Fichier;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-//        if (args.length != 2) {
-//            System.err.println("Usage: java compression.Main <input.txt> <output.huff>");
-//             System.exit(1);
-//        }
-//        String input = args[0];
-//        String output = args[1];
-
-        String input = "data/data_code/structured_1000000_5.py";
-        String output= "data/data_code/structured_1000000_5.huff";
+        if (args.length != 2) {
+            System.err.println("Usage: java compression.Main <input.txt> <output.huff>");
+             System.exit(1);
+        }
+        String input = args[0];
+        String output = args[1];
 
         long start = System.currentTimeMillis();
         Compression.compresser(input, output);
